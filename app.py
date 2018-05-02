b9b22e3693dfddbddca42014518c7740ae1c6c0e
kevin <17730804096@189.cn>
2018-05-02 13:29:07 +0800

    init

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import os
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+
+    COV = coverage.coverage(branch=True, include='flaskr/*')
+    COV.start()
+
+import sys
+import click
 from flask_migrate import Migrate
 
 from flaskr import create_app, db
@@ -12,3 +21,28 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db)
+
+
+@app.cli.command()
+@click.option('--coverage/--no-coverage', default=False, help='Enable code coverage')
+def test(coverage):
+    """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import subprocess
+        os.environ['FLASK_COVERAGE'] = '1'
+        sys.exit(subprocess.call(sys.argv))
+
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
--- flaskr/main/views.py
+++ flaskr/main/views.py
@@ -1,6 +1,19 @@
+from flask import current_app, abort, request
+
 from . import main
+
+
+@main.route('/shutdown')
+def shutdown():
+    if not current_app.testing:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'shutting down...'
 
 
 @main.route('/')
 def index():
-    return 'hello flask!'
+    return '<h1>hello flask!</h1>'
