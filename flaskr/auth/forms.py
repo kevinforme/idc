b5ecb60d29a1a1b3baf4c81e505f4a3a178d2666
kevin <17730804096@189.cn>
2018-05-13 21:08:48 +0800

    version1.0

--- flaskr/auth/forms.py
+++ flaskr/auth/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
-    username = StringField('用户名', validators=[DataRequired(), Length(6, 64)])
+    username = StringField('用户名', validators=[DataRequired(), Length(1, 64)])
     password = StringField('密码', validators=[DataRequired(), Length(6, 64)])
     submit=SubmitField('提交')
